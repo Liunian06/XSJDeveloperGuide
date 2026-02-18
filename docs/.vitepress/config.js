c458e92999708b53c85c808e94e2d7a31ddd9b41
Liunian06 <277914121@qq.com>
2026-02-18 16:11:01 +0800

    Initial commit: VitePress project setup

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'XSJ Developer Guide',
+  description: 'XSJ 开发者指南',
+  base: '/XSJDeveloperGuide/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' }
+    ]
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Hello World
+
+欢迎来到 XSJ Developer Guide！
