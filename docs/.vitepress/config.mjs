5a24e15f902ae73fcc6a48163100704c8709614a
Liunian06 <277914121@qq.com>
2026-02-19 10:53:46 +0800

    恢复到简单的 Hello World 版本

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'XSJ Developer Guide',
+  description: 'XSJ Developer Guide',
+  base: '/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' }
+    ],
+    sidebar: [],
+    socialLinks: []
+  }
+})
